599f4413afb03a71a3df116ede40f05674c87a52
6-MillionBees <BoettcherAr321@northwested.org>
2024-09-11 10:04:06 -0400

    Update individual-concat-practice.py

--- individual-concat-practice.py
+++ individual-concat-practice.py
@@ -3,14 +3,14 @@
 # String Concatenation
 
 # Part 1
-# Use concatenation to build and display a string that displays which city and state you live in
+# Use concatenation to build and display a string that displays which city and state you live 
 
-'''
+
 city = 'Traverse City, '
 state = 'Michigan'
 
 print('I live in ' + city + state)
-'''
+
 
 # Part 2
 # Create a custom message that welcomes the user by first name to a game you created
@@ -18,32 +18,31 @@ print('I live in ' + city + state)
 # Create another variable that stores the welcome message
 # Use concatenation to create the customized message
 
-'''
+
 print('What\'s your first name?')
 
 first_name = str(input())
 
 print('Welcome ' + first_name + ' to Adventuria!')
-'''
+
 
 # Part 3
 # Assign a number to your age variable
 # Use the built-in Python str( ) function to convert your age to a string (when doing concatenation)
 # Use concatenation to display a sentence that tells us your first name and age
 
-'''
+
 age = 16
 
 print('I am ' + str(age) + ' years old.')
-'''
+
 
 # Part 4
 # Use an f-string to build and display a string that contains your first name, last name, and your height in inches
 
-'''
+
 first_name2 = 'Arden'
 last_name = 'Boettcher'
 height = 'roughly 67'
 
 print(f'My name is {first_name2} {last_name} and I am {height} inches tall ')
-'''
