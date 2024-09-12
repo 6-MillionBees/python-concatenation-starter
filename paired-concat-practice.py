674a9d98323d9d0b00e0b109f0ec3f40d4aa39b3
6-MillionBees <BoettcherAr321@northwested.org>
2024-09-12 14:06:37 -0400

    Update paired-concat-practice.py

--- paired-concat-practice.py
+++ paired-concat-practice.py
@@ -10,19 +10,30 @@
 # Create variables to store your first, middle, and last name
 # Have Python print a message that contains your concatenated full name, i.e., your combined first, middle and last names
 
+first_name = 'Arden '
+middle_name = 'Elis'
+last_name = 'Boettcher'
 
+print(first_name + middle_name + ' ' + last_name)
 
 # Part 2
 # Assume you're building a Space Invaders game
 # Use concatenation to create and display a custom welcome message that includes the player's first name
 
+player_name = input('What\'s your name? ')
 
+print('Welcome ' + player_name.title() + ' to Space Invaders')
 
 # Part 3
 # Use concatenation to build and display a string that includes your first name, last name, and the year you were born
 
+year_of_birth = '2008'
 
+print('my name is ' + first_name + middle_name + ' ' + last_name + ' ' + 'and I was born in ' + year_of_birth) 
 
 # Part 4
 # Use concatenation to create and display a sentence that says which country in the world
 # you would visit if money were no object
+
+country = 'finland'
+
